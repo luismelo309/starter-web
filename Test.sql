a194c47409b24fd2d624a07413e55f034aeda4f9
l.melo <l.melo@urios.com>
2022-05-18 12:02:44 +0100

    Teste SQL

---
+++ Test.sql
@@ -0,0 +1,10 @@
+﻿/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [NoFiche]
+      ,[SIREN]
+      ,[NoOrigin]
+      ,[Data]
+      ,[DtCreation]
+      ,[NoDataOnDemand]
+      ,[CodeEntreprise]
+  FROM [DODUrios].[dbo].[Fiches]
+  order by DtCreation asc
